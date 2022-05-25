8091389fd9292f3ce6301c0dc215866b0dea7bef
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-05-25 15:48:54 -0300

    Yelp MVP challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+mero = Restaurant.create(name: 'Mero', address: 'Av. Dona Renata 123', phone_number: '(19)3551-0112', category: 'italian')
+naoto = Restaurant.create(name: 'Naoto', address: 'Av. Dona Renata 5849', phone_number: '(19)3541-8745', category: 'japanese')
+paris = Restaurant.create(name: 'Paris 6', address: 'Av. Paulista 606', phone_number: '(11)8548-7596', category: 'french')
+liberdade = Restaurant.create(name: 'Liberade', address: 'Av. liberdade 789', phone_number: '(11)7412-8596', category: 'chinese')
+greenhouse = Restaurant.create(name: 'Greenhouse', address: 'R. Fernão Dias, 672', phone_number: '(11)8749-4452', category: 'belgian')
